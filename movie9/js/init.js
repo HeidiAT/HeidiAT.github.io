1234a311ecf00ea80e3384685d05a82cae3f1d06
HeidiAT <angeltengriano@gmail.com>
2021-01-19 10:55:04 +0800

    coba

--- movie9/js/init.js
+++ movie9/js/init.js
@@ -5,8 +5,3 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
-
-  var elem = document.querySelector('.collapsible.expandable');
-  var instance = M.Collapsible.init(elem, {
-    accordion: false
-  });
