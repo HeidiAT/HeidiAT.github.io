f4b1f67df110777438b572f742f3ec25ffa92ba5
HeidiAT <angeltengriano@gmail.com>
2021-01-29 20:46:45 +0800

    perbaikan movie9\js\tmdb.js

--- movie9/js/tmdb.js
+++ movie9/js/tmdb.js
@@ -40,7 +40,16 @@ fetch(endpoint_url+ "movie/" + movie_id + "?api_key=" + key + "&language=en-US&p
  .then(json)
  .then(function(data) {
   var movinfo = "";
-  //movie overview
+
+  var genre = "";
+    for (i = 0; i < data.genres.length; i++) {
+        genre += data.genres[i].name + ", ";
+    }
+  var proname = "";
+  for (i = 0; i < data.production_companies.length; i++) {
+      proname += data.production_companies[i].name + ", ";
+  }
+
   movinfo += `
   <div id="index-banner" class="parallax-container">
     <div class="section no-pad-bot">
@@ -61,61 +70,57 @@ fetch(endpoint_url+ "movie/" + movie_id + "?api_key=" + key + "&language=en-US&p
   <div class="container">
     <div class="col s12 m5">
       <div class="card">
-      <div class="card-image waves-effect waves-block waves-light z-depth-3">
-        <img src="https://image.tmdb.org/t/p/w500${data.poster_path}"/>
+        <div class="card-image waves-effect waves-block waves-light z-depth-3">
+          <img src="https://image.tmdb.org/t/p/w500${data.poster_path}"/>
+        </div>
       </div>
     </div>
-    </div>
-
-  <div class="col m7 s12">
-  <table class="">
-  <thead>
-    <tr>
-        <th>Nama Film</th>
-        <td class="grey-text text-darken-2">${data.title}</td>
-    </tr>
-    <tr>
-    <th>Tanggal</th>
-      <td class="grey-text text-darken-2">${data.release_date}</td>
-    </tr>
-    <tr>
-      <th>Budget</th>
-      <td class="grey-text text-darken-2">US$ ${data.budget}</td>
-    </tr>
-    <tr>
-      <th>Bahasa</th>
-      <td class="grey-text text-darken-2"> ${data.original_language}</td>
-    </tr>
-    <tr>
-      <th>Pendapatan</th>
-      <td class="grey-text text-darken-2">US$ ${data.revenue}</td>
-    </tr>
-    <tr>
-      <th>Durasi Film</th>
-      <td class="grey-text text-darken-2"> ${data.runtime} Menit</td>
-    </tr>
-    <tr>
-      <th>Status</th>
-      <td class="grey-text text-darken-2"> ${data.status}</td>
-    </tr>
-    <tr>
-      <th>Homepage</th>
-      <td><a href="${data.homepage}" class="customlink">${data.homepage}</a></td>
-    </tr>
-  </thead>
-  <tbody>
-  </tbody>
-</table>
-<div class="col m12 s12 sinopsis" style="border:2px;">
-  <h3>Sinopsis Cerita </h3>
-  <p>${data.overview}</p>
-</div>
+      <strong>${data.title}</strong>
+    <div class="col m7 s12">
+    <table class="">
+      <thead>
+        <tr>
+          <th>Tanggal Rilis</th>
+          <td class="grey-text text-darken-2">${data.release_date}</td>
+        </tr>
+        <tr>
+          <th>Popularitas</th>
+          <td class="grey-text text-darken-2"> ${data.popularity}</td>
+        </tr>
+        <tr>
+          <th>Bahasa</th>
+          <td class="grey-text text-darken-2"> ${data.original_language}</td>
+        </tr>
+        <tr>
+          <th>Genre</th>
+          <td class="grey-text text-darken-2"> ${genre}</td>
+        </tr>
+        <tr>
+          <th>Durasi Film</th>
+          <td class="grey-text text-darken-2"> ${data.runtime} Menit</td>
+        </tr>
+        <tr>
+          <th>Status</th>
+          <td class="grey-text text-darken-2"> ${data.status}</td>
+        </tr>
+        <tr>
+          <th>Di Produksi Oleh</th>
+          <td class="grey-text text-darken-2">${proname}</td>
+        </tr>
+        <tr>
+          <th>Homepage</th>
+          <td><a href="${data.homepage}" class="customlink">${data.homepage}</a></td>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    </table>
+      <h3>Sinopsis Cerita </h3>
+      <p>${data.overview}</p>
   </div>
   </div>
-`;
+  `;
       document.getElementById("movie_detail").innerHTML = movinfo;
-})
-
+    })
   .catch(error);
-
 }
