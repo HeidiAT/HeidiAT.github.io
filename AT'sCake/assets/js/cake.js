126cd4e45dddd39e60415872efcb5eadf10f9fdc
HeidiAT <angeltengriano@gmail.com>
2021-02-20 21:08:52 +0800

    perbaruan link api

--- AT'sCake/assets/js/cake.js
+++ AT'sCake/assets/js/cake.js
@@ -1,4 +1,4 @@
-const endpoint_url="http://51018011.p-web.local/api";
+const endpoint_url="http://51018011.p-web.click/backend/api";
 
 function getResepKering() {
   fetch(endpoint_url + "/resep/kuekering")
